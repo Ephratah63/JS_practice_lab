1b8610bbc6d75233342cb3cf15255d3c54db82db
Ephratah63 <ephratah.girma@aastustudent.edu.et>
2026-06-28 02:15:57 +0300

    Updated the todo list project using arrays and loops mainly within the javascript.

--- To_do_list/script.js
+++ To_do_list/script.js
@@ -1,2 +1,34 @@
-let calculation='';
-console.log(calculation);
+const todoList = ["do the laundry", "prepare lunch"];
+listingOnPage();
+function listingOnPage() {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<div>
+      <p>${todo}</p>
+      <button class="delete_btn" onclick="
+        todoList.splice(${i}, 1);
+        listingOnPage();
+        "><input class="text" type="date" value="2026-02-18">
+        <img 
+          src="images/icon_delete.png" alt="delete icon" style="width: 20px; height: 12px">
+      </button>
+    </div>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+  document.querySelector(".todo_list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".name-input");
+  const name = inputElement.value;
+  console.log(name);
+
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = "";
+
+  listingOnPage();
+}
